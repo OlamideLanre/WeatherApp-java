c8f9751fda98475fe80a84e26e848cab979e4eec
Ola <lanreprincess@gmail.com>
2024-05-03 19:06:53 +0100

    landing page done

--- app/src/main/java/my/first/weather/LandingPage.java
+++ app/src/main/java/my/first/weather/LandingPage.java
@@ -2,13 +2,27 @@ package my.first.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
+
+    Button getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        getStarted=findViewById(R.id.getStartedBtn);
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(LandingPage.this, MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
